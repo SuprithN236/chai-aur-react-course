8172d259f09f446a69b81473a50f2c68b291f6bf
SuprithN236 <suprithn236@gmail.com>
2024-05-06 08:13:48 +0530

    megaProject-Day-1

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+// you are creating a class here, you will have all the methods for authentication inside this.
+class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async signup({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return this.account.deleteSessions();
+    } catch (error) {
+      console.log("appwrite service :: logout :: error", error);
+    }
+  }
+}
+
+// we need an object to use the methods of the class. so we are creating and exporting one here only so we can directly use the methods associated with it.
+const authService = new AuthService();
+
+export default authService;
---
+++ MegaBlog/src/conf/conf.js
@@ -0,0 +1,10 @@
+const config = {
+  // you are converting the variable values to string even if it is string to make sure that there is no chance for error. this is a good practice to follow
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default config;
